f9ace489911cc94784efaf972c773869286aeb3f
Paolo Manca <paolomanca@gmail.com>
2022-07-26 17:47:52 +0200

    put shebang in line n. 1

--- soluzioni/1.1.0_1_loop_numeri_naturali.sh
+++ soluzioni/1.1.0_1_loop_numeri_naturali.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 #SFIDA N. 1
 
 #Usa i cicli per visualizzare i numeri naturali da 1 a 50.
 #Si aspetta un output del tipo:
 
 #1.1.0 creazione ciclo dei numeri da 1 a 50
-#!/bin/bash
+
 #creare un ciclo che come condizione, crei una variabile $i che deve partire con valore 1,
 # non essere maggiore di 50 ed infine che incrementi di 1 il valore di $i ogni volta che si ripete il ciclo. 
 for (( i=1 ; i<=50 ; i++  ));
