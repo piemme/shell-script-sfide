c531c24b84da273950a978476b2c4e917d2c655c
GersonYanez <gersonalbertoyanez@gmail.com>
2022-07-26 09:24:31 +0200

    aggiunto commenti

--- soluzioni/1.1.0_1_loop_numeri_naturali.sh
+++ soluzioni/1.1.0_1_loop_numeri_naturali.sh
@@ -5,7 +5,12 @@
 
 #1.1.0 creazione ciclo dei numeri da 1 a 50
 #!/bin/bash
+#creare un ciclo che come condizione, crei una variabile $i che deve partire con valore 1,
+# non essere maggiore di 50 ed infine che incrementi di 1 il valore di $i ogni volta che si ripete il ciclo. 
 for (( i=1 ; i<=50 ; i++  ));
 do
+#il comando 'echo' stampa nel terminale il contenuto, in questo caso, una stringa e il valore di $i 
     echo "numero naturale: $i"
 done
+#al termine del ciclo si ha una lista di numeri naturali, dal 1 al 50
+
