1d61396e9a4d684d06752c3f03573e16120bf617
GersonYanez <gersonalbertoyanez@gmail.com>
2022-07-26 10:57:58 +0200

    aggiunto commento al es 5

--- soluzioni/1.1.0_5_backup_buckup.sh
+++ soluzioni/1.1.0_5_backup_buckup.sh
@@ -7,3 +7,8 @@
 
 #!/bin/bash -x
 find /home/gerson -mtime -2 -exec tar -cvzaf 25.07.2022.tar.gz {} \;
+#conoscenze richieste: 
+# 'find'(trova ogni dato riguardante la directory, come il '-mtime' che trova i dati riguadanti i file modificati);
+# 'exec'(permette l'iserimento di un altro comando che va a modificare l'output di find);
+# 'tar'(creazione archivio);
+# '-cvzaf'(opszioni del comando 'tar'), (-c) crea un archivio , (-v) manda nel display le informazioni, (-z) comprime il file, (-a) unisci i file i un unico archivio, (-f) permette l'iserimento del nome del archivio;
