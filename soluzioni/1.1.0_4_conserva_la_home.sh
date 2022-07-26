384cbe9cdad5800e21aada08776f5d5cdb2e988f
GersonYanez <gersonalbertoyanez@gmail.com>
2022-07-26 09:48:59 +0200

    aggiunto commento es 4

--- soluzioni/1.1.0_4_conserva_la_home.sh
+++ soluzioni/1.1.0_4_conserva_la_home.sh
@@ -9,11 +9,14 @@
 
 ls $HOME/* > 'copia_home.txt'
 echo "comprimo il file" 
+#qui si presenta una condizione che controlla l'esistenza del file, se è vera lo elimina.
 [ -f copia_home.txt.gz ] && rm copia_home.txt.gz
 gzip -k copia_home.txt
 echo "view del file"
 cat copia_home.txt
 
-
+#questo esercizio prevede la conoscenza del comando 'ls'(stampa il contenuto dela cartella selezionata)
+# e saper decidere l'uscita del suo output, 
+# 'gzip' (comprimi), 'cat' (stampa il contenuto del file selezionato).
 
 
