f24e1188577d5725dfb70410e7412b5dea0d50ab
GersonYanez <gersonalbertoyanez@gmail.com>
2022-08-13 00:54:19 +0200

    correzione terzo es

--- soluzioni/1.1.0_3_comparazione_numeri.sh
+++ soluzioni/1.1.0_3_comparazione_numeri.sh
@@ -6,20 +6,31 @@
 #- X maggiore Y
 #- X uguale Y
 
+#-----------------------------------------------------------------------------------------------------
 
-echo "Inserisci il primo numero: .."
+
+echo "Inserisci il valore: "
 read X
-echo "inserisci il secondo numero: .."
+echo "inserisci il secondo valore: "
 read Y
+
+# 'if' se il valore di x è minore di y 
 if [[ $X < $Y ]]
 then
+    # stampa la condizione
     echo "$X è minore di $Y"
+# 'elif' se x è maggiore di y
 elif [[ $X > $Y ]]
 then
+    # stampa la condizione
     echo "$X è maggiore di $Y"
+# se i valori sono uguale 
 elif [[ $X == $Y ]]
 then
+    #stampa questa condizione
     echo "$X è uguale a $Y"
-fi
+fi # conclusione del ciclo
+
+#-------------------------------------------------------------------------------------------------------
 #prendendo d'esempio l'esercizio 2 , questo esercizio richiedeva le stesse conoscenze.
 #utilizzo del comando 'echo', 'read', creazioni di variabili e del ciclo 'if'
