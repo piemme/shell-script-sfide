d856313288226c43737717c8beacd80be8555821
GersonYanez <gersonalbertoyanez@gmail.com>
2022-09-09 12:06:27 +0200

    modifiche es.5 e 4

--- soluzioni/prova.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#SFIDA N. 5 #1.2.1
-#find ~ -mtime -2 | xargs -0 tar -czavf "$(date +"%m-%d-%y")".tar.gz
-#tar -czavf "$(date +"%d-%m-%y")".tar.gz | xargs -0 find ~ -mtime -2
-ls -1 | xargs -0 find ~ -amin -2 
