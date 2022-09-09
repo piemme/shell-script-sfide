e06629cc77edd5b8383fdeb5b9aa70b4deee2457
GersonYanez <gersonalbertoyanez@gmail.com>
2022-09-09 12:16:15 +0200

    es.5 commenti

--- soluzioni/5_backup_backup.sh
+++ soluzioni/5_backup_backup.sh
@@ -11,11 +11,6 @@ tar -cvzf "$(date +"%m-%d-%y")".tar.gz | xargs find ~ -type f -mtime -2
 # ------------------------------------------------------------------------
 # traduzione dei comandi usati: 
 # 'find'(trova ogni dato riguardante la directory, unito al '-mtime -2' quelli modificati nelle ultime 48h);
-<<<<<<< HEAD
-# '~' indica la dirctory (Home/NOME-UTENTE).
-# 'exec'(permette l'iserimento di un altro comando che va a modificare l'output di find);
-=======
 # 'xargs'(permette il passaggio del output di find al comando tar);
->>>>>>> dev
 # 'tar'(creazione archivio unito al '-cvzaf' crea un archivio, comprime i file, li unisce in un unico archivio e devi inserire il nome del archivio);
 #find ~ -type f -mtime -2 
